de60fa5058d5ea03643ddbb9ed8196e45e2ecd11
AartiPundalik <aartipundalik68@gmail.com>
2023-04-15 08:55:21 +0530

    [Aarti] Add : UC5 - Example Of Encapsulation

---
+++ Oops_Programs/Encapsulation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_Programs
+{
+    public class Encapsulation
+    {
+            public String PersonName;
+            public int PersonAge;
+
+            public String Name
+            {
+                get { return PersonName; }
+
+                set { PersonName = value; }
+            }
+
+            public int Age
+            {
+                get { return PersonAge; }
+
+                set { PersonAge = value; }
+            }
+        }
+    }
+
--- Oops_Programs/Program.cs
+++ Oops_Programs/Program.cs
@@ -12,30 +12,30 @@ namespace Oops_Programs
     {
         static void Main(string[] args)
         {
-            //// Types Of Inheritance
-            //Stationary stationary = new Stationary();
-            //stationary.NoteBook();
-            //Pen pen = new Pen();
-            //pen.BluePen();
-            //CraftPaper craftpaper = new CraftPaper();
-            //craftpaper.VerticleCraftPaper();
-            //WritingMaterial material = new Envelops();
-            //material.Classmate();
+            // Types Of Inheritance
+            Stationary stationary = new Stationary();
+            stationary.NoteBook();
+            Pen pen = new Pen();
+            pen.BluePen();
+            CraftPaper craftpaper = new CraftPaper();
+            craftpaper.VerticleCraftPaper();
+            WritingMaterial material = new Envelops();
+            material.Classmate();
 
-            //// Class And Object
-            //Employee e1 = new Employee();
-            //Employee e2 = new Employee();
-            //e1.insert(65, "Aarti", 800000f);
-            //e2.insert(66, "Sangita", 400000f);
-            //e1.display();
-            //e2.display();
-            //Console.ReadLine();
+            // Class And Object
+            Employee e1 = new Employee();
+            Employee e2 = new Employee();
+            e1.insert(65, "Aarti", 800000f);
+            e2.insert(66, "Sangita", 400000f);
+            e1.display();
+            e2.display();
+            Console.ReadLine();
 
-            //// Inheritance
-            //Truck truck = new Truck();
-            //truck.EngineNo();
-            //Console.WriteLine(truck.brand + " " + truck.modelName);
-            //Console.ReadLine();
+            // Inheritance
+            Truck truck = new Truck();
+            truck.EngineNo();
+            Console.WriteLine(truck.brand + " " + truck.modelName);
+            Console.ReadLine();
 
             // Polymorphism
             Polymorphism polymorphismflower = new Polymorphism();
@@ -56,6 +56,15 @@ namespace Oops_Programs
             polygon = new Square();
             polygon.render();
             Console.ReadLine();
+
+            // Encapsulation
+            Encapsulation encapsulation = new Encapsulation();
+            encapsulation.Name = "Aarti Pundalik";
+            encapsulation.Age = 22;
+            Console.WriteLine(" Name : " + encapsulation.Name);
+            Console.WriteLine(" Age : " + encapsulation.Age);
+            Console.ReadLine();
+
 
 
         }

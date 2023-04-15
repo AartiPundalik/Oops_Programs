4f9cba388f3fdba0a8ce3bbf4eb8559bacd66b7c
AartiPundalik <aartipundalik68@gmail.com>
2023-04-15 08:48:11 +0530

    [Aarti] Add : UC4 - Example of Polymorphism And Their Types

---
+++ Oops_Programs/Polymorphism.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oops_Programs
+{
+    public class Polymorphism
+    {
+        public void FlowerSmells()
+        {
+            Console.WriteLine("Flowers Smells");
+        }
+    }
+
+    class Lily : Polymorphism
+    {
+        public void FlowerSmells()
+        {
+            Console.WriteLine("Lily Smells So Nostalgic");
+        }
+    }
+
+    class Jasmine : Polymorphism
+    {
+        public void FlowerSmells()
+        {
+            Console.WriteLine("Jasmine Smells so Refreshing");
+        }
+    }
+
+
+    // Types Of Polymorphism
+
+    public class IntergerNumber                     // Method Overloading
+    {
+
+        public void TotalSum(int a, int b)
+        {
+            Console.WriteLine("The sum of numbers is " + (a + b));
+        }
+
+        public void totalSum(double a, double b)
+        {
+            Console.WriteLine("The sum of numbers is " + (a + b));
+        }
+
+    }
+
+
+    public class Polygon                               // Method Overriding
+    {
+
+        public virtual void render()
+        {
+            Console.WriteLine("Rendering Polygon...");
+        }
+    }
+
+    class Square : Polygon
+    {
+
+        public override void render()
+        {
+            Console.WriteLine("Rendering Square...");
+        }
+    }
+}
+
+
+    
+
--- Oops_Programs/Program.cs
+++ Oops_Programs/Program.cs
@@ -12,31 +12,50 @@ namespace Oops_Programs
     {
         static void Main(string[] args)
         {
-            // Types Of Inheritance
-            Stationary stationary = new Stationary();
-            stationary.NoteBook();
-            Pen pen = new Pen();
-            pen.BluePen();
-            CraftPaper craftpaper = new CraftPaper();
-            craftpaper.VerticleCraftPaper();
-            WritingMaterial material = new Envelops();
-            material.Classmate();
+            //// Types Of Inheritance
+            //Stationary stationary = new Stationary();
+            //stationary.NoteBook();
+            //Pen pen = new Pen();
+            //pen.BluePen();
+            //CraftPaper craftpaper = new CraftPaper();
+            //craftpaper.VerticleCraftPaper();
+            //WritingMaterial material = new Envelops();
+            //material.Classmate();
 
-            // Class And Object
-            Employee e1 = new Employee();
-            Employee e2 = new Employee();
-            e1.insert(65, "Aarti", 800000f);
-            e2.insert(66, "Sangita", 400000f);
-            e1.display();
-            e2.display();
+            //// Class And Object
+            //Employee e1 = new Employee();
+            //Employee e2 = new Employee();
+            //e1.insert(65, "Aarti", 800000f);
+            //e2.insert(66, "Sangita", 400000f);
+            //e1.display();
+            //e2.display();
+            //Console.ReadLine();
+
+            //// Inheritance
+            //Truck truck = new Truck();
+            //truck.EngineNo();
+            //Console.WriteLine(truck.brand + " " + truck.modelName);
+            //Console.ReadLine();
+
+            // Polymorphism
+            Polymorphism polymorphismflower = new Polymorphism();
+            polymorphismflower.FlowerSmells();
+            Polymorphism flowerLily = new Lily();
+            flowerLily.FlowerSmells();
+            Polymorphism flowerJasmine = new Jasmine();
+            flowerJasmine.FlowerSmells();
             Console.ReadLine();
-
-            // Inheritance
-            Truck truck = new Truck();
-            truck.EngineNo();
-            Console.WriteLine(truck.brand + " " + truck.modelName);
+            // Method Overloading
+            IntergerNumber intergerNumber = new IntergerNumber();
+            intergerNumber.TotalSum(5, 7);
+            intergerNumber.TotalSum(53, 8);
             Console.ReadLine();
-
+            // Method Overriding
+            Polygon polygon = new Polygon();
+            polygon.render();
+            polygon = new Square();
+            polygon.render();
+            Console.ReadLine();
 
 
         }
